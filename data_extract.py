49b80ffa19dcd6b1cab3679dee89bdfb6525e6aa
Snehil Verma <snehilverma@Snehils-MacBook-Air.local>
2022-03-19 23:27:12 -0400

    first commit

---
+++ data_extract.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+import random
+
+file_name = 'text_classification_dataset.xlsx'
+dfs = pd.read_excel(file_name, sheet_name=None)
+# print(dfs)
+df = dfs['Sheet1']
+print(df.type.unique())
+values = ['medical','entertainment']
+df = df[df.type.isin(values) == False]
+rows = df.sample(n=120)
+print(rows)
+
+rows = rows.drop('type',1)
+writer = pd.ExcelWriter('output.xlsx')
+rows.to_excel(writer)
+writer.save()
