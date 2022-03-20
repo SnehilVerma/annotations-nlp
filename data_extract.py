97aeaaecc5ee1ac26a0f3243fcde38598618033c
Snehil Verma <snehilverma@Snehils-MacBook-Air.local>
2022-03-20 12:50:20 -0400

    added new annotator file

--- data_extract.py
+++ data_extract.py
@@ -7,12 +7,13 @@ dfs = pd.read_excel(file_name, sheet_name=None)
 # print(dfs)
 df = dfs['Sheet1']
 print(df.type.unique())
+
 values = ['medical','entertainment']
 df = df[df.type.isin(values) == False]
 rows = df.sample(n=120)
 print(rows)
 
 rows = rows.drop('type',1)
-writer = pd.ExcelWriter('output_deb.xlsx')
+writer = pd.ExcelWriter('output_shriya.xlsx')
 rows.to_excel(writer)
 writer.save()
